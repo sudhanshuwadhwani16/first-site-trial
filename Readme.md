fa8d1226c0508d8255edf9dbea16871e4ff8be73
Sudhanshu Wadhwani <77379202+sudhanshuwadhwani16@users.noreply.github.com>
2021-01-30 19:24:59 +0530

    docs readme

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# student trial
+
+Hey! This is sudhanshu
---
+++ exa.py
@@ -0,0 +1,14 @@
+
+
+lis = []
+
+n = int(input("Enter the number of elements"))
+
+for i in range(0,n):
+    ele = int(input())
+
+    lis.append(ele)
+
+c = input("Enter the type of sorting")
+
+print(lis)
